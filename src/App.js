2305a1c5ebad73e9700cf54b65b394e75a3c1b24
Jimijos <jimijos@gmail.com>
2020-11-27 11:17:13 +0000

    circular component with container gameboard and game scene

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./styles.css";
 //components
 import Header from "./components/header/Header";
+import GameBoard from "./components/gameboard/GameBoard";
+ 
 
-
-export default function App() {
-  const showRules = ({target: {name}}) => {
-    console.log(name);
-    }   
+export default function App() {  
   return (
     <div className="App">
       <Header />
+      <GameBoard />
     </div>
   );
 }
