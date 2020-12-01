df6308cbe354e1efc697f96d0da6d2262aa37a43
yunusfirat <38798014+yunusfirat@users.noreply.github.com>
2020-12-01 03:39:16 +0000

    rules button has been added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import "./styles.css";
+//components
+import Header from "./components/header/Header";
+import GameBoard from "./components/gameboard/GameBoard";
+import RulesButton from './components/rules/RulesButton';
+import BasicRules from './components/rules/basicrules/BasicRules'
+ 
+
+
+
+export default function App() {  
+  const [rulesClicked, setRulesClicked] = useState(false);
+  const showRules = () => {
+    setRulesClicked(!rulesClicked);
+    }   
+  return (
+    <div className="App">
+      <Header />
+      <GameBoard />
+      <RulesButton handleClick={showRules}/>
+      <BasicRules rulescard={rulesClicked} handleClick={showRules}/>
+    </div>
+  );
+}
